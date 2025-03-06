5d357bcc877c9a4244f6e28a1646ac816b4f8472
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-06 15:59:45 +0300

    SweetAlert 2 npm ve swalservice kurulumu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { BlankComponent } from '../blank/blank.component';
-import { SectionComponent } from '../section/section.component';
 import { SharedModule } from '../../modules/shared.module';
+import { SwalService } from '../../services/swal.service';
 
 @Component({
   selector: 'app-home',
@@ -11,4 +10,11 @@ import { SharedModule } from '../../modules/shared.module';
 })
 export class HomeComponent {
 
+  constructor(
+    private swal: SwalService
+  ){
+    this.swal.callSwal("Kaydı sil","Kaydı silmek istiyor musunuz ?", () => {
+      alert("Silme işlemi başarılı");
+    })
+  }
 }
