0c4f284ed0bb823136a83df4251aa9eda69815b5
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-06 17:22:54 +0300

    Login page tasarımı

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { LoginModel } from '../../models/login.model';
+import { SharedModule } from '../../modules/shared.module';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  model: LoginModel = new LoginModel();
+  signIn(){
+    
+  }
 }
