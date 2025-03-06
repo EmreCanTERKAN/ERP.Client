8583a7cb850a03c1d01c02d9552b652b3ff6abf5
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-06 14:11:47 +0300

    Main Sidebar için Menü listesi

--- src/app/components/layouts/main-sidebar/main-sidebar.component.ts
+++ src/app/components/layouts/main-sidebar/main-sidebar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Menus } from '../../../menu';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MenuPipe } from '../../../pipes/menu.pipe';
 
 @Component({
   selector: 'app-main-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive ,FormsModule,MenuPipe],
   templateUrl: './main-sidebar.component.html',
   styleUrl: './main-sidebar.component.css'
 })
 export class MainSidebarComponent {
-
+  search : string = "";
+  menus = Menus;
 }
