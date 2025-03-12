9837495f7b0a9dee8d748108e9f229daa32344f7
EmreCanTERKAN <emrecanterkan06@gmail.com>
2025-03-12 20:49:28 +0300

    footer current time

--- src/app/components/layouts/footer/footer.component.ts
+++ src/app/components/layouts/footer/footer.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit, OnDestroy {
+  currentTime: string = '';
+  private intervalId: any;
 
+  ngOnInit(): void {
+    this.updateTime(); // İlk değeri ayarla
+    this.intervalId = setInterval(() => {
+      this.updateTime();
+    }, 1000); // Her saniye güncelle
+  }
+
+  updateTime() {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString('tr-TR'); // Saat formatı: 20:44:01
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId); // Bileşen yok olursa intervali temizle
+  }
 }
